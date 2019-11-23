f247d7be1693035c7ec517dafdbfafd6a31505c9
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-11-23 21:48:39 +0100

    Structure de données jaccard dans Graph pour la closeness centrality

--- src/Online/Closeness.java
+++ src/Online/Closeness.java
@@ -1,9 +1,20 @@
 package Online;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Closeness {
     public static void closeness(Graph g){
-        ArrayList<EdgeJacquard> jacquard = g.getJacquard();
+        HashMap<Integer, HashSet<EdgeJacquard<Integer>>> jacquard = g.getJacquard();
+        Double[] sumJacquard = new Double[g.nbS];
+
+        for(int i=0; i<g.nbS; i++) {
+            /*double sum = jacquard
+                    .get(i)
+                    .map(e -> Math.min(e.getA(), e.getB()))
+                    .reduce(0, (e1, e2) -> e1 + e2)
+                    .doubleValue();*/
+        }
     }
 }
