7638ece47a70a728bfe404c14349f154e75d45a9
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-11-24 13:45:53 +0100

    Commencement des tests.

---
+++ src/Online/Tests/ClosenessTest.java
@@ -0,0 +1,10 @@
+package Online.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClosenessTest {
+
+    @org.junit.jupiter.api.Test
+    void closeness() {
+    }
+}
---
+++ src/Online/Tests/HeapSortTest.java
@@ -0,0 +1,12 @@
+package Online.Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeapSortTest {
+
+    @Test
+    void heapsort() {
+    }
+}
