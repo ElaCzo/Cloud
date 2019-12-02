821e9be8750de3509e7799c38dfc84325a6b555d
Ivan <delgado.ivan@protonmail.com>
2019-12-02 13:05:39 +0100

    mise au propre

--- src/Online/Online.java
+++ src/Online/Online.java
@@ -39,23 +39,23 @@ public class Online {
         Graph g = new Graph(docs);
         // System.out.println(g);
 
-        // System.out.println("\n###### closeness ######\n");
+        System.out.println("\n###### closeness ######\n");
 
-        // // affichage par ordre décroissant
-        // int[] closeness = HeapSort.heapsort(Closeness.closeness(g));
-        // for (int i = docs.size() - 1; i >= 0; i--) {
-        //     System.out.println("doc : " + docs.get(closeness[i]).nom);
-        // }
+        // affichage par ordre décroissant
+        int[] closeness = HeapSort.heapsort(Closeness.closeness(g));
+        for (int i = docs.size() - 1; i >= 0; i--) {
+            System.out.println("doc : " + docs.get(closeness[i]).nom);
+        }
 
-        // System.out.println("\n###### betwenness ######\n");
+        System.out.println("\n###### betwenness ######\n");
 
-        // int[] betw = HeapSort.heapsort(Betweeness.CalculateBetweeness(g));
-        // // affichage par ordre décroissant
-        // for (int i = docs.size() - 1; i >= 0; i--) {
-        //     System.out.println("doc : " + docs.get(betw[i]).nom);
-        // }
+        int[] betw = HeapSort.heapsort(Betweeness.CalculateBetweeness(g));
+        // affichage par ordre décroissant
+        for (int i = docs.size() - 1; i >= 0; i--) {
+            System.out.println("doc : " + docs.get(betw[i]).nom);
+        }
 
-        System.out.println("\n###### Centrality ######\n");
+        System.out.println("\n###### new Centrality ######\n");
 
         // affichage par ordre décroissant
         int[] cent = HeapSort.heapsort(Centrality.calcCentrality(g));
@@ -63,13 +63,13 @@ public class Online {
             System.out.println("doc : " + docs.get(cent[i]).nom);
         }
 
-        // System.out.println("\n###### pageRank ######\n");
+        System.out.println("\n###### pageRank ######\n");
 
-        // int[] pagerank = Rank.pageRank(g, 0.15);
-        // // affichage par ordre décroissant
-        // for (int i = docs.size() - 1; i >= 0; i--) {
-        //     System.out.println("doc : " + docs.get(pagerank[i]).nom);
-        // }
+        int[] pagerank = Rank.pageRank(g, 0.15);
+        // affichage par ordre décroissant
+        for (int i = docs.size() - 1; i >= 0; i--) {
+            System.out.println("doc : " + docs.get(pagerank[i]).nom);
+        }
 
         Display.registerEdgesInFile("jaccard.txt", g);
     }
