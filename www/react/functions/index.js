4804da54e16b9ea442e1f6d4d548a7f22b005bf1
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-01-14 21:32:17 +0100

    Maj Firestore automatiquement.

--- www/react/functions/index.js
+++ www/react/functions/index.js
@@ -1,4 +1,9 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,20 +16,20 @@ exports.firestoreUpdate = functions.storage.object().onFinalize(async (object) =
   console.log(object);
 });
 
-exports.onFileChange = functions.storage.object().onFinalize(event => {
+/*exports.onFileChange = functions.storage.object().onFinalize(event => {
   console.log(event);
   return;
-});
+});*/
 
 exports.addAllFilesInFireStore = functions.https.onRequest((request, response) => {
-  var storageRef = firebase.storage();
+  var storageRef = admin.storage();
 
   storageRef.listAll().then(function (result) {
     result.items.forEach(function (docRef) {
  
         let data = {
           title: docRef.name,
-          url: docRef.fullPath 
+          path: docRef.fullPath 
         };
 
         let setDoc = db.collection('livres').doc().set(data);
