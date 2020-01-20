eb3a557a21569ab195d8790aea7c3da6dd76e10d
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-01-20 15:47:32 +0100

    WIP

--- src/recherche/Search.java
+++ src/recherche/Search.java
@@ -44,8 +44,36 @@ public class Search {
         }
     }
 
-    public static ArrayList<String> test(String val){
-        ArrayList<String> books = new ArrayList<>();
+    public static ArrayList<HashMap<String, String>> search(String val){
+        ArrayList<HashMap<String, String>> books = new ArrayList<>();
+
+        String rechercheString = val;
+        ArrayList<String> paths = new ArrayList<>();
+        HashMap<String, String> titres = titre.loadTitres();
+        try {
+
+            Files.walk(Paths.get("./data/index/")).filter(Files::isRegularFile).map(p -> p.toString())
+                    .filter(p -> p.endsWith("index")).collect(Collectors.toList()).forEach(x -> paths.add(x));
+
+            for (String path : paths) {
+                // System.out.println("test");
+                HashMap<String, Integer> indexMap = Indexing.loadIndexMap(path);
+                if (indexMap.containsKey(rechercheString)) {
+
+                    String textString = path.substring(0, path.indexOf("/index"));
+                    textString+="/books/"+path.substring(path.lastIndexOf("/")+1 , path.lastIndexOf("."));
+                    HashMap<String, String> h = new HashMap();
+                    books.add(h);
+                    h.put("title", titres.getOrDefault(textString, textString));
+                    h.put("path", textString);
+                }
+            }
+
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+
         return books;
     }
 }
