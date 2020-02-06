b1345017c094dfecefa81c1a276cc0aa5fb285bc
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-02-06 02:11:37 +0100

    Maj du nombre de livres pour les tests

--- script.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-    cat $1 | 
-    #tr '[:upper:]' '[:lower:]' |
-    awk -F "[^A-Za-z]" '{for(i=1;i<=NF;i++){ {print tolower($i)} } }' |
-    awk 'length($0)>3' |sort |sed 's/[[:space:]]*$//'| uniq -c | sort -n > $2 
- 
