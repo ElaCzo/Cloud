34c90c4ab27e3cd7a399575642a9e354e362878c
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2020-02-09 20:40:09 +0100

    Clean code

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-heroku restart
-https://mysterious-oasis-90910.herokuapp.com/searchbooks?search=pirate
+API sur heroku : https://mysterious-oasis-90910.herokuapp.com/searchbooks?search=pirate
 
 react-script start dans www/react/
+pour le site ReactJS
 
+
+lien github : https://github.com/ElisabethAbbasZadeh/Cloud.git
---
+++ www/react/build/precache-manifest.a334d0237376d42323fdf50722fc4fcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46802bf827e4c145c329639a044904e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d48b2c7ae2217b175e",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d983d53791bf932dd8e7",
+    "url": "/static/css/main.16de8f73.chunk.css"
+  },
+  {
+    "revision": "82d48b2c7ae2217b175e",
+    "url": "/static/js/2.575ff47b.chunk.js"
+  },
+  {
+    "revision": "7c9e406c7e5de3f85ea5fe369b055434",
+    "url": "/static/js/2.575ff47b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d983d53791bf932dd8e7",
+    "url": "/static/js/main.7de81d8d.chunk.js"
+  },
+  {
+    "revision": "4f296a1fbf135809dcef",
+    "url": "/static/js/runtime-main.0206be14.js"
+  }
+]);
